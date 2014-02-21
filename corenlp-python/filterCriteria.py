881826063e70d0e1f781b3b3a73b9ba697f0a0df
jesustilapia <aml373@cornell.ecu>
2014-02-21 15:32:27 -0500

    --
    still not working

--- corenlp-python/filterCriteria.py
+++ corenlp-python/filterCriteria.py
@@ -6,19 +6,27 @@ db=MySQLdb.connect(host='eltanin.cis.cornell.edu', user='annotator',passwd='Ann0
 c=db.cursor()
 c.execute('SELECT char_annotation, a_id from Annotations WHERE doc_id = %s'%(doc_id,))
 rows=c.fetchall()
-
-for row in rows:
-  (char_annotation, ann_id) = row
+if (len(rows) == 0):
+  print("no annotations selected with doc_id =%s"%(doc_id))
+  return None 
+for (i = 0; i < len(rows); i++):
+  (char_annotation, ann_id) = rows[i]
   char_ans = char_annotation.split() #split on whitespace
   def splitOnPeriod(str): #split on period
     str.split('.')
-  charPairs = map(splitOnPeriod, char_ans)
-  for charPairs
+  #split each annotation start-end pair by period, then convert to int from unicode
+  charPairs = map(int, map(splitOnPeriod, char_ans))
+  highlight_lens = []
+  for (j = 0; j < len(charPairs); j++):
+    if (len(charPairs[j] == 2)):
+      highlight_lens[j] = charPairs[j][1] - charPair[j][0]
+  #average character length of words
+  avgLen[i] = sum(highlight_lens)/len(highlight_lens)
 
 
 '''
-  @param bool criteria evaluates whether or not a doc passes a criteria
-  @docs  list of doc_id's 
+  @param criteria: boolean function to evaluate docs 
+  @param docs: list of doc_id's 
   return pair of doc lists (passCriteria, failCriteria)
   where 'passCriteria' is the list of docs in which criteria(doc) is true
   and   'failCriteria' is the rest of the docs that did not pass the criteria
@@ -32,16 +40,39 @@ def filterDocs(criteria, docs):
 
 
 '''
-return true if the average word-length of the annotations
+return true if the average char-length of the annotations
 in doc is less than avglen  
 '''
 def shortAnnotations(doc, avglen): 
+
+  
   #for a range of characters that are highlighted, find number of words that are highlighted
   def annotationLength(char_annotation):
+#get annotations for a given doc_id
+#taken from fextractor.py
+db=MySQLdb.connect(host='eltanin.cis.cornell.edu', user='annotator',passwd='Ann0tateTh!s', db='FrameAnnotation', charset='ascii')
+c=db.cursor()
+c.execute('SELECT char_annotation, a_id from Annotations WHERE doc_id = %s'%(doc_id,))
+rows=c.fetchall()
+if (len(rows) == 0):
+  print("no annotations selected with doc_id =%s"%(doc_id))
+  return None 
+for (i = 0; i < len(rows); i++):
+  (char_annotation, ann_id) = rows[i]
+  char_ans = char_annotation.split() #split on whitespace
+  def splitOnPeriod(str): #split on period
+    str.split('.')
+  #split each annotation start-end pair by period, then convert to int from unicode
+  charPairs = map(int, map(splitOnPeriod, char_ans))
+  highlight_lens = []
+  for (j = 0; j < len(charPairs); j++):
+    if (len(charPairs[j] == 2)):
+      highlight_lens[j] = charPairs[j][1] - charPair[j][0]
+  #average character length of words
+  avgLen[i] = sum(highlight_lens)/len(highlight_lens)
+    
 
-  #count the number of spaces
-
-  #test that this actually returns corret number of words
+    #test that this actually returns corret number of words
 
 '''
 return a pair of doc lists (passCriteria, failCriteria)
