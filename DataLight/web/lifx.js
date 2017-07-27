52b1f67bffc3845963aac768eff0640d30693b0c
ericpsb <eric.baumer@gmail.com>
2017-07-27 09:20:41 -0400

    node-lifx started
    Beginnings of integration to use node-lifx library.

--- DataLight/web/lifx.js
+++ DataLight/web/lifx.js
@@ -1,5 +1,14 @@
-// embeded in submit button, load facebook and twitter login page
-function allWhite() {
-    window.alert("all White");
-}
 
+function listLights() {
+    return $.ajax({
+        url: "https://api.lifx.com/v1/lights/all",
+        type: "get",
+        headers: {
+            Authorization: "Bearer c54a000420aa2543b2abd0ea5f7a65d7bfc51a8a3b275013da646043c1fa5192"
+        },
+        dataType: 'json',
+        success: function(data, status){
+            return alert("Data: " + data + "\nStatus: " + status);
+        }
+    });
+}
